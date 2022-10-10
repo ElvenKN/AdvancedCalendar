fdf09d3673ac3bc30bd0df81501d4985dfd7b674
Gonzalo Diethelm <gonzo@fb.com>
2022-10-10 13:12:36 +0200

    Add useless endpoint for month/year

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,29 @@
-const express = require( "express" );
+const express = require('express');
+const bodyParser = require('body-parser');
+const url = require('url');
+const querystring = require('querystring');
+
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello nico!" );
-} );
+app.get("/", (req, res) => {
+  res.send("Hello nico!");
+});
 
-app.get( "/bye", ( req, res ) => {
-    res.send( "Bye nico!" );
-} );
+app.get("/bye", (req, res) => {
+  res.send("Bye nico!");
+});
+
+app.get('/month', async function(req, res) {
+  let y = req.query.y;
+  let m = req.query.m;
+
+  res.send(`Got month ${m} and year ${y}`)
+});
+
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
