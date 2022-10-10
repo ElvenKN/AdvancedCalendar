8ba702b085865aa61bf786a0ecf75d7834b83274
Gonzalo Diethelm <gonzo@fb.com>
2022-10-10 13:38:49 +0200

    Add a mock fixed month calendar

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,75 @@ app.get('/month', async function(req, res) {
   let y = req.query.y;
   let m = req.query.m;
 
-  res.send(`Got month ${m} and year ${y}`)
+  let grid = `
+<p>October 2022</p>
+<table border=1>
+ <tr>
+  <td>Mon</td>
+  <td>Tue</td>
+  <td>Wed</td>
+  <td>Thu</td>
+  <td>Fri</td>
+  <td>Sat</td>
+  <td>Sun</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td>1</td>
+  <td>2</td>
+ </tr>
+ <tr>
+  <td>3</td>
+  <td>4</td>
+  <td>5</td>
+  <td>6</td>
+  <td>7</td>
+  <td>8</td>
+  <td>9</td>
+ </tr>
+ <tr>
+  <td>10</td>
+  <td>11</td>
+  <td>12</td>
+  <td>13</td>
+  <td>14</td>
+  <td>15</td>
+  <td>16</td>
+ </tr>
+ <tr>
+  <td>17</td>
+  <td>18</td>
+  <td>19</td>
+  <td>20</td>
+  <td>21</td>
+  <td>22</td>
+  <td>23</td>
+ </tr>
+ <tr>
+  <td>24</td>
+  <td>25</td>
+  <td>26</td>
+  <td>27</td>
+  <td>28</td>
+  <td>29</td>
+  <td>30</td>
+ </tr>
+ <tr>
+  <td>31</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+ </tr>
+</table>
+`;
+  res.send(grid);
 });
 
 
