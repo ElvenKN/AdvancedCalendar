5234e0caa8375f59bade0d5ee0c65371376752fc
nico <115456996+ElvenKN@users.noreply.github.com>
2022-10-10 15:20:14 +0200

    changed hardcoded month into a function

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const url = require('url');
 const querystring = require('querystring');
+const dayjs = require ('dayjs');
 
 const app = express();
 const port = 8080; // default port to listen
@@ -19,74 +20,7 @@ app.get('/month', async function(req, res) {
   let y = req.query.y;
   let m = req.query.m;
 
-  let grid = `
-<p>October 2022</p>
-<table border=1>
- <tr>
-  <td>Mon</td>
-  <td>Tue</td>
-  <td>Wed</td>
-  <td>Thu</td>
-  <td>Fri</td>
-  <td>Sat</td>
-  <td>Sun</td>
- </tr>
- <tr>
-  <td></td>
-  <td></td>
-  <td></td>
-  <td></td>
-  <td></td>
-  <td>1</td>
-  <td>2</td>
- </tr>
- <tr>
-  <td>3</td>
-  <td>4</td>
-  <td>5</td>
-  <td>6</td>
-  <td>7</td>
-  <td>8</td>
-  <td>9</td>
- </tr>
- <tr>
-  <td>10</td>
-  <td>11</td>
-  <td>12</td>
-  <td>13</td>
-  <td>14</td>
-  <td>15</td>
-  <td>16</td>
- </tr>
- <tr>
-  <td>17</td>
-  <td>18</td>
-  <td>19</td>
-  <td>20</td>
-  <td>21</td>
-  <td>22</td>
-  <td>23</td>
- </tr>
- <tr>
-  <td>24</td>
-  <td>25</td>
-  <td>26</td>
-  <td>27</td>
-  <td>28</td>
-  <td>29</td>
-  <td>30</td>
- </tr>
- <tr>
-  <td>31</td>
-  <td></td>
-  <td></td>
-  <td></td>
-  <td></td>
-  <td></td>
-  <td></td>
- </tr>
-</table>
-`;
+  let grid = createMonthGrid(y, m);
   res.send(grid);
 });
 
@@ -138,6 +72,22 @@ app.get('/day', async function(req, res) {
 `;
   res.send(grid);
 });
+
+function createMonthGrid(y, m) {
+  console.log(`y: [${y}], m: [${m}]`)
+  const date = dayjs(`${y}-${m}-01`, 'YYYY-MM-DD');
+  console.log(date)
+  const dow = date.day(); // 0: Sun, 1: Mon, ..., 6: Sat
+  const eom = date.endOf('month').date()
+  console.log(`dow: [${dow}], eom: [${eom}]`)
+
+  let grid = '';
+  for (let d = 1 ; d <= eom ; d += 1) {
+    grid = grid + d + ',';
+  }
+
+  return grid;
+}
 
 
 // start the Express server
